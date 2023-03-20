404a236a422cd6ab192b03c6d5878d6f5127a75f
tom <1728178781@qq.com>
2023-03-20 19:24:22 +0800

    first commit

---
+++ my-product-service/src/main/java/com/example/myproductservice/MyProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.myproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProductServiceApplication.class, args);
+    }
+
+}
---
+++ my-product-service/src/test/java/com/example/myproductservice/MyProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
